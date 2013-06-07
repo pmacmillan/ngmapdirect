04b0b44b3a7a82a859af69490114694f41a30bbc
David Chase <davidchase03@gmail.com>
2013-06-07 14:18:12 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,91 @@
+/*global google:false */
+var component = angular.module('mapComponent', []);
+
+component.directive('map', function () {
+    'use strict';
+    var directionsDisplay = new google.maps.DirectionsRenderer(),
+        directionsService = new google.maps.DirectionsService(),
+        geocoder = new google.maps.Geocoder(),
+        map,
+        marker,
+        mapObj,
+        infowindow;
+
+    mapObj = {
+        restrict: 'EAC',
+        scope: {
+            destination: '@',
+            markerContent: '@',
+            zoom: '=',
+            type: '@',
+            directions: '@'
+        },
+        replace: true,
+        templateUrl: 'map.html',
+        link: function (scope, element) {
+            scope.init = function () {
+                var mapOptions = {
+                    zoom: scope.zoom !== undefined ? scope.zoom : 15,
+                    mapTypeId: scope.type !== undefined ? scope.type.toLowerCase() : 'roadmap',
+                    streetViewControl: false
+                };
+                map = new google.maps.Map(document.getElementById('theMap'), mapOptions); // todo: use angular-element :)      
+                scope.endPoint = scope.destination !== undefined ? scope.destination : '1600 Amphitheatre Parkway, Santa Clara County, CA';
+
+                geocoder.geocode({
+                    address: scope.endPoint
+                }, function (results, status) {
+                    var location = results[0].geometry.location;
+                    if (status === google.maps.GeocoderStatus.OK) {
+                        map.setCenter(location);
+                        marker = new google.maps.Marker({
+                            map: map,
+                            position: location,
+                            animation: google.maps.Animation.DROP
+                        });
+                        infowindow = new google.maps.InfoWindow({content: scope.markerContent !== undefined ? scope.markerContent : 'Google HQ'});
+                        google.maps.event.addListener(marker, 'click', function () {
+                            return infowindow.open(map, marker);
+                        });
+
+                    } else {
+                        alert('Cannot Geocode');
+                    }
+
+                });
+               
+
+            };
+
+            scope.init();
+
+            scope.getDirections = function () {
+                var request = {
+                    origin: scope.origin,
+                    destination: scope.endPoint,
+                    travelMode: google.maps.DirectionsTravelMode.DRIVING
+                };
+                directionsService.route(request, function (response, status) {
+                    return status === google.maps.DirectionsStatus.OK ? directionsDisplay.setDirections(response) : console.warn(status);
+                });
+                directionsDisplay.setMap(map);
+
+                directionsDisplay.setPanel(document.getElementById('directionsList')); // again need to use angular element thats ugly otherwise.
+            };
+
+            scope.clearDirections = function () {
+                scope.init();
+                directionsDisplay.setPanel(null);
+                scope.origin = '';
+            };
+
+
+
+        }
+    };
+
+    return mapObj;
+
+
+
+});
